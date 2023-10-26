7785e88dda05e20d3f5d770e7220245044d60e3c
khareena786 <96183829+khareena786@users.noreply.github.com>
2023-10-26 05:01:30 -0700

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace library
+{
+    public class books
+    {
+        private string title;
+        private string author;
+        private int ISBN;
+
+        public books(string title, string author, int ISBN)
+        {
+            this.title = title;
+            this.author = author;
+            this.ISBN=ISBN;
+        }
+
+        public string booktitle
+        {
+            get { return title; }
+        }
+        public string bookauthor
+        {
+            get { return author; }
+
+        }
+        public int bookisbn
+        {
+            get { return ISBN; }
+        }
+         public string Tostring()
+        {
+            return ($"{title} ,{author},{ISBN}");
+            
+        }
+ 
+        public class LibraryInventory<T> where T : Book
+        {
+            private List<T> books;
+
+            public LibraryInventory()
+            {
+                books = new List<T>();
+            }
+
+            public void addbook(T book)
+            {
+                books.Add(book);
+            }
+
+            public void removebook(T book)
+            {
+                books.Remove(book);
+            }
+
+            public T bookby_ISBN(string isbn)
+            {
+                foreach (T b in books)
+                {
+                    if (b.ISBN == isbn)
+                    {
+                        return b;
+                    }
+                }
+                return null; // Book not found
+            }
+
+            public void displaybooks()
+            {
+                foreach (T book in books)
+                {
+                    Console.WriteLine(book);
+                }
+            }
+        }
+
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static library.books;
+
+namespace library
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LibraryInventory<Book> library = new LibraryInventory<Book>();
+
+            Book book1 = new Book("Book1", "Author1", 123456);
+            Book book2 = new Book("Book2", "Author2", 789012);
+
+            library.addbook(book1);
+            library.addbook(book2);
+
+            Console.WriteLine("All Books:");
+            library.displaybooks();
+
+            int isbnToFind = 123456;
+            Book foundBook = library.bookby_ISBN(isbnToFind);
+            if (foundBook != null)
+            {
+                Console.WriteLine("\nFound Book:");
+                Console.WriteLine(foundBook);
+            }
+            else
+            {
+                Console.WriteLine($"\nBook with ISBN {isbnToFind} not found.");
+            }
+        }
+    }
+}
+    
+
